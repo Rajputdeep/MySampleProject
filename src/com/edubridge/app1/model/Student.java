d704e18e2b5a633a7c4155d55d84f1077fe583e4
91762 <91762@DippalRajput9011>
2023-10-18 17:39:43 +0530

    chages done in Student.java

--- src/com/edubridge/app1/model/Student.java
+++ src/com/edubridge/app1/model/Student.java
@@ -1,5 +1,29 @@
 package com.edubridge.app1.model;
 
 public class Student {
+	private int employeeId;
+	private String employeeName;
+	private int employeeAddress;
+	
+	//Constructor using field
+	public Student(int employeeId, String employeeName, int employeeAddress) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+		this.employeeAddress = employeeAddress;
+	}
+
+	
+	//ToString Method
+	@Override
+	public String toString() {
+		return "Student [employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeAddress="
+				+ employeeAddress + "]";
+	}
+	
+	
+	
+	
+	
 
 }
